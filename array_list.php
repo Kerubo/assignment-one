6fb53c616f1a95d6cbcd22fd29d006a0bdd22888
Kerubo <lucykerubo763@gmail.com>
2014-06-12 12:45:17 +0300

    Create array_list.php

---
+++ array_list.php
@@ -0,0 +1,9 @@
+<?php
+return $friends=array (
+	array("name"=>"Rahim", "age"=>"9","phone"=>"07082745707"),
+	array ("name"=>"Cynthia","age"=>"4","phone"=>"0723471310"),
+	array("name"=>"Daflyne","age"=>"8","phone"=>"0708567392"),
+	 array("name" =>"Bevon","age"=>"13","phone"=>"0708936457" )
+	 );
+ 
+?>
