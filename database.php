94f62cd49adf2c8ed7a4c86c4b5c1885496528c4
Kerubo <lucykerubo763@gmail.com>
2014-06-12 12:44:19 +0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,25 @@
+<?php require database.php?>
+//
+<?php 
+
+if(!$dbconnect = mysql_connect('localhost', 'root', 'akirachix')) {
+   echo "Here are some of my friends 'localhost'.";
+   exit;
+} // if
+if (!mysql_select_db('test')) {
+   echo "Cannot connect to database 'test'";
+   exit;
+} // if
+
+$table_id = 'some_table';
+$query = "SELECT * FROM $table_id";
+$dbresult = mysql_query($query, $dbconnect);
+
+<form action="index.php" method="post">
+Name: <input type="text" name="name"><br>
+Age: <input type="text" name="age"><br>
+Phone: <input type="text" name="phone"><br>
+<input type="submit">
+</form>
+
+?>
