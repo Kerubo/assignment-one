ab3beebe2cb97a51b9e5ad9dd40ec9b0d6fcd8da
Kerubo <lucykerubo763@gmail.com>
2014-06-12 12:42:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php require 'array_list.php';?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+	
+	</head>
+<body>
+<h2>My friends</h2>
+<div>
+<table array_list>
+	
+	<thead>
+	
+		<tr>
+		<td>Name</td>
+		<td>Age</td>
+		<td>Phone</td>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($friends as $friends) : ?>
+		
+	
+	 <tr> 
+	 <td> <?=$friends['name']?></td>
+	 <td><?=$friends['age']?></td>
+	  <td><?=$friends['phone']?></td>
+	  </tr>
+	  <?php endforeach ?>
+	  </tbody>
+</table>
+</div>
+</body>
+</html>
