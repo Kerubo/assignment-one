57d2b33968ceb0c4eed1d0b6e2eb4a68c3ad2952
Kerubo <lucykerubo763@gmail.com>
2014-07-12 06:10:10 +0300

    quiz2

---
+++ assignment-one/quiz2/listcar.php
@@ -0,0 +1,14 @@
+<?php
+//creating an array of car models
+ return $models = array(
+                 array("Type"=>"Pick-up","Name"=>"Subaru","Color"=>"Blue");
+                 array("Type"=>"Lorry","Name"=>"Subaru","Color"=>"Yellow");
+                 array("Type"=>"Bus","Name"=>"Subaru","Color"=>"Green");
+                 array("Type"=>"Nissan","Name"=>"Subaru","Color"=>"Orange");
+                 array("Type"=>"Four Wheel Drive","Name"=>"Subaru","Color"=>"Pink");
+                 array("Type"=>"Motorbike","Name"=>"Subaru","Color"=>"Maroon");
+                 array("Type"=>"Bicycle","Name"=>"Subaru","Color"=>"Purple");
+);               array("Type"=>"Mkokoteni","Name"=>"Subaru","Color"=>"Red");
+     
+
+?>
